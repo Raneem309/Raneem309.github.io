f9ee3017a245d0ab18ced44fd9ba404748ce652e
Raneem <raneem309@gmail.com>
2025-01-07 18:27:21 -0500

    test1

--- script.js
+++ script.js
@@ -10,45 +10,9 @@ window.onload = function () {
 
 function loadInitialData() {
   let bio =
-    "Hi, I'm Raneem Ali, a dedicated and detail-oriented software and web developer based in Brooklyn, NY. With a solid foundation in programming languages such as JavaScript, HTML, CSS, Python, and Java, I specialize in creating scalable, responsive, and user-friendly web applications. My passion for problem-solving and innovation drives me to continuously learn and refine my technical expertise. I worked as a Junior Developer at Agility Consultants, where I collaborated with cross-functional teams to develop and maintain high-performance web applications. Leveraging JavaScript, HTML, and CSS, I optimized user experiences by ensuring responsiveness and cross-browser compatibility. I also implemented unit testing, debugging, and deployment strategies, ensuring the reliability and scalability of software solutions. In addition to technical development, I have experience using Agile methodologies and version control systems like GitHub, enabling efficient workflows and collaborative project delivery. My exposure to cloud computing platforms such as AWS during my studies and Azure in the workplace has given me a solid understanding of their capabilities. Prior to my development role, I honed my leadership and organizational skills as a First Line Manager at The Grooming Lounge, where I oversaw team operations, optimized database systems, and implemented streamlined workflows. This experience strengthened my ability to lead teams, train staff, and deliver results under tight deadlines. I earned my degree in Management Information Systems from George Mason University, where I gained hands-on experience with programming languages, cloud computing, and system management. My time there provided me with a strong foundation in software development principles and problem-solving techniques. With a commitment to building clean, functional, and scalable solutions, I am eager to bring my technical expertise and passion for development to a dynamic team. Let's connect and create innovative solutions together!";
-  aboutMeBio.innerHTML = bio;
-}
+`
+Hi, I'm Raneem Ali, a dedicated and detail-oriented software and web developer based in Brooklyn, NY. With a solid foundation in programming languages such as JavaScript, HTML, CSS, Python, and Java, I specialize in creating scalable, responsive, and user-friendly web applications. My passion for problem-solving and innovation drives me to continuously learn and refine my technical expertise.\n
 
-function tabLinks() {
-  // Adding onclick for each tab
-  document.querySelectorAll(".tab").forEach((tab) => {
-    tab.addEventListener("click", function () {
-      handleTabs(this.id);
-    });
-  });
-}
+I worked as a Junior Developer at Agility Consultants, where I collaborated with cross-functional teams to develop and maintain high-performance web applications. Leveraging JavaScript, HTML, and CSS, I optimized user experiences by ensuring responsiveness and cross-browser compatibility. I also implemented unit testing, debugging, and deployment strategies, ensuring the reliability and scalability of software solutions.\n
 
-function handleTabs(tabId) {
-  // Hide all sections by default
-  const sections = document.querySelectorAll("section");
-  sections.forEach((section) => {
-    section.classList.add("displayNone");
-  });
-
-  // Show the corresponding section based on tab click
-  if (tabId === "aboutMeTab") {
-    aboutMeSection.classList.remove("displayNone");
-    mainHeader.classList.add("displayNone");
-    mainHeader.style.position = "fixed";
-  } else if (tabId === "projectTab") {
-    projectsSection.classList.remove("displayNone");
-    mainHeader.classList.add("displayNone");
-    mainHeader.style.position = "fixed";
-  } else if (tabId === "contactTab") {
-    contactSection.classList.remove("displayNone");
-    mainHeader.classList.add("displayNone");
-    mainHeader.style.position = "fixed";
-  } else if (tabId === "homeTab") {
-    mainHeader.classList.remove("displayNone");
-    homeTab.classList.add("displayNone");
-    mainHeader.style.position = "";
-    sections.forEach((section) => {
-      section.classList.add("displayNone");
-    });
-  }
-}
+In addition to technical development, I have experience using Agile methodologies and version control systems like GitHub, enabling efficient workflows and collaborative project deliv
