6cdac660e3761b9bb1de632aa16eb10b894e785b
Raneem <raneem309@gmail.com>
2025-02-11 19:30:57 -0500

    test bround

--- script.js
+++ script.js
@@ -74,9 +74,6 @@ function handleOpeningScreen() {
   const openName = document.getElementById("openName");
   const openTitle = document.getElementById("openTitle");
 
-  // Show the opening screen immediately.
-  openingScreen.classList.remove("displayNone");
-
   // Fade in the name immediately.
   setTimeout(() => { 
     openName.style.opacity = "1"; 
@@ -85,24 +82,25 @@ function handleOpeningScreen() {
   // Fade in the title after 1 second.
   setTimeout(() => { 
     openTitle.style.opacity = "1"; 
-  }, 1000);
+  }, 2000);
 
   // Delay the fade-out of the text to 7 seconds (instead of 5).
   setTimeout(() => { 
     openName.style.opacity = "0"; 
     openTitle.style.opacity = "0";
-  }, 7000);
+  }, 10000);
 
   // Delay the start of the container fade-out until 7 seconds.
   setTimeout(() => {
-    openingScreen.style.transition = "opacity 6s ease";
+    openingScreen.style.transition = "opacity 8s ease";
     openingScreen.style.opacity = "0";
 
     // After the 6-second fade-out, hide the container.
     setTimeout(() => { 
       openingScreen.classList.add("displayNone"); 
-    }, 6000);
-  }, 7000);
+    }, 8000);
+  }, 10000);
+  
 }
 
 
